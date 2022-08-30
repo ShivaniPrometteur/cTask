0ed1b347025ffcdf39beccfa6d9a5b728e5b855b
unknown <ShivaniPrometteur>
2022-08-30 14:13:52 +0530

    changes for leap year and time done

--- q2.js
+++ q2.js
@@ -96,6 +96,9 @@
 
 
 
+
+
+
 function abc(arr){
 
     let arr1=[]
@@ -103,21 +106,16 @@ function abc(arr){
        for(let i=0;i<arr.length;i++){
            let dob=Date.parse(arr[i].other[0].birthday);
           // console.log(dob)
-           let day=Date.parse("2022/8/20")
+           let day=Date.parse("2022-08-20 12:00:00")
    
-           var ageinmilliseconds = day - dob; 
+          
            
            
-           var milliseconds=ageinmilliseconds;
-           var second=1000;
-           var minute=second*60;
-           var hour=minute*60;
-           var dayy=hour*24;
-           var month=dayy*30;
-           var year=dayy*365;
+           var milliseconds=day - dob;
+          
            
            
-           var age=Math.floor(milliseconds/year);    //if we use Math.round() then final output will be [20,18,18]
+           var age=Math.floor(milliseconds/31557600000);    //if we use Math.round() then final output will be [20,18,18]
                  
           
            
@@ -216,6 +214,137 @@ function abc(arr){
    ];
    
    console.log(abc(people_array));
+
+
+
+
+
+
+
+
+
+
+
+   // function abc(arr){
+
+//     let arr1=[]
+   
+//        for(let i=0;i<arr.length;i++){
+//            let dob=Date.parse(arr[i].other[0].birthday);
+//           // console.log(dob)
+//            let day=Date.parse("2022/8/20")
+   
+//            var ageinmilliseconds = day - dob; 
+           
+           
+//            var milliseconds=ageinmilliseconds;
+//            var second=1000;
+//            var minute=second*60;
+//            var hour=minute*60;
+//            var dayy=hour*24;
+//            var month=dayy*30;
+//            var year=dayy*365;
+           
+           
+//            var age=Math.floor(milliseconds/year);    //if we use Math.round() then final output will be [20,18,18]
+                 
+          
+           
+           
+//            if(age>=18 && age!=58 && age<58){
+//                arr1.push(age)
+//            } 
+//        }
+//        return arr1;
+   
+//    }
+   
+   
+//    const people_array = [
+   
+//      {
+   
+//        name: 'sahil',
+   
+//        other: [
+   
+//          {
+   
+//            birthday: '10 August 2002',
+   
+//          },
+   
+//        ],
+   
+//      },
+   
+//      {
+   
+//        name: 'sneha',
+   
+//        other: [
+   
+//          {
+   
+//            birthday: '1 January 2006',
+   
+//          },
+   
+//        ],
+   
+//      },
+   
+//      {
+   
+//        name: 'minal',
+   
+//        other: [
+   
+//          {
+   
+//            birthday: '1 February 2005',
+   
+//          },
+   
+//        ],
+   
+//      },
+   
+//      {
+   
+//        name: 'snehal',
+   
+//        other: [
+   
+//          {
+   
+//            birthday: '3 March 1960',
+   
+//          },
+   
+//        ],
+   
+//      },
+   
+//      {
+   
+//        name: 'raj',
+   
+//        other: [
+   
+//          {
+   
+//            birthday: '1 January 2005',
+   
+//          },
+   
+//        ],
+   
+//      },
+   
+//    ];
+   
+//    console.log(abc(people_array));
 
 
 
